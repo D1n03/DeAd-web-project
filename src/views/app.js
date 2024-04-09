642fe61ab9672eda1ac2da4e7b7a747154f6afc4
D1n03 <bubble03red@gmail.com>
2024-04-09 14:31:08 +0300

    added submenu for profile icon

--- src/views/app.js
+++ src/views/app.js
@@ -5,3 +5,17 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+
+let subMenu = document.getElementById("subMenu");
+let person = document.getElementById("person-icon");
+
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu");
+}
+
+document.onclick = function(e) {
+    if (!subMenu.contains(e.target) && !person.contains(e.target)) {
+        subMenu.classList.remove("open-menu");
+    }
+}
